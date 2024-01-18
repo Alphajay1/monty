f3dc1dbd412a9d0cb440efa14d5d11f1c5ef5ab0
Alphajay1 <ayakojamesty@gmail.com>
2024-01-18 11:30:49 -0800

    opcodes_instruction2.c

---
+++ opcodes_instruction2.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * nop - doesn't do anything
+ * @head: head of the linked list
+ * @line_number: line number
+ *
+ * Return: void
+ */
+void nop(stack_t **head, unsigned int line_number)
+{
+	(void)head;
+	(void)line_number;
+}
